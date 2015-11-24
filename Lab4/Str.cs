4da3b101fe5b38e3c4f997ab100b41d12849b823
Ilshat Galimov <einnks@gmail.com>
2015-11-24 22:16:32 +0300

    Str class created & >> operator is overloaded

---
+++ Lab4/Str.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab4
+{
+    class Str
+    {
+        public string Value { get; }
+
+        public Str(string str)
+        {
+            Value = str;
+        }
+
+        public Str(Str str)
+        {
+            Value = str.Value;
+        }
+
+        public static Str operator >>(Str str, int x)
+        {
+            // var st = new string(str.Value.ToCharArray().Reverse().ToString());
+            return new Str(str.Value.ToCharArray().Reverse().ToString());
+        }
+    }
+}
